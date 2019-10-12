c892802cf605a287aedd04152be114dd3b06ca1a
VorapratR <vorapratrattana@gmail.com>
2019-10-13 02:47:43 +0700

    [Ch.9] User Management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'top-stories', loadChildren: () => import('./top-stories/top-stories.module').then( m => m.TopStoriesModule)},
   { path: 'comments/:id', loadChildren: () => import('./comments/comments.module').then(m => m.CommentsModule)},
+  { path: 'auth/', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
   { path: '', redirectTo: '/top-stories', pathMatch: 'full' },
 ];
 
