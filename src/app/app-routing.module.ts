8597b109ad92db0dd069a243218d6d5f05886469
VorapratR <vorapratrattana@gmail.com>
2019-10-13 21:30:22 +0700

    [Ch.10] add heart button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AuthGuard } from './auth/services/auth-guard.service';
+import { AuthGuard } from './auth/services/auth-guard.service';
 
 const routes: Routes = [
   { path: 'top-stories', loadChildren: () => import('./top-stories/top-stories.module').then( m => m.TopStoriesModule)},
   { path: 'comments/:id', loadChildren: () => import('./comments/comments.module').then(m => m.CommentsModule)},
-  // { path: 'favorites', loadChildren: 'app/favorites-list/favorites-list.module#FavoritesListModule', canActivate: [AuthGuard] },
+  { path: 'favorites', loadChildren: () => import('./favorites-list/favorites-list.module').then(m => m.FavoritesListModule)},
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
   { path: '', redirectTo: '/top-stories', pathMatch: 'full' },
 ];
